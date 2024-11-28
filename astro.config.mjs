672f5a6fbe3e0d039e2819520699f25ef133a698
Stiven Villarreal <stivenvillarreal@gmail.com>
2024-11-28 13:19:59 -0500

    Remove Vercel adapter configuration and engines field from package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+// import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server",
-  adapter: vercel(),
+  // output: "server",
+  // adapter: vercel(),
 });
